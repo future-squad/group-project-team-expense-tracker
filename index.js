90c0a0b2a3d00f1d4e0f9465e2775291d25cf7a4
Ali haider <alihaidergr8@gmail.com>
2025-10-17 00:40:50 +0500

    Done with expenses task

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log(`hey `)
+
+let expenses = [];
+
+function add_Expense(amount, description, paidby) {
+    if (!members.includes(paidby.toLowerCase())) {
+        console.log(`Error: member: ${paidby} doesn't exist`)
+        return false;
+    }
+
+    const expense = {
+        amount: amount,
+        description: description,
+        paidby: paidby
+    };
+
+    expenses.push(expense);
+    console.log("Expenses added successfully!")
+    return expense;
+}
+
+function listexpenses() {
+    console.table(expenses);
+}
+
+add_Expense(300,"lunch","haider");
+add_Expense(500,"breaki","hassan");
+add_Expense(900,"coffee","ismail");
+add_Expense(100,"dinner", "adnan");
+add_Expense(560,"steak","ali")
+
+
+
+
+listexpenses ();
+
+
+
+
+
+
+
