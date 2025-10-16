5c571b939c2d8c661c51777f836872f0849d48dc
Haider-Ali127 <haidet0000@gmail.com>
2025-10-16 16:26:02 +0500

    Added my member management feature

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log(`hey `)
+//  Feature-Member
+let members = [];
+function addmember(name) {
+    let lowername = name.toLowerCase();
+    for (let member of members) {
+        if (member.toLowerCase() === lowername) {
+            console.log(`❌ ${name} was already exist`)
+            return;
+        }
+    }
+    members.push(name)
+    console.log("✅Member Added:", name)
+}
+addmember("adnan")
+addmember("sara")
+addmember("ali")
+addmember("hussain")
+addmember("hassan")
+addmember("adnan")
+addmember("imran")
+addmember("imran")
+
+function tabular(members) {
+    console.table(members.map(s => {
+        return s;
+    }))
+}
+tabular(members)
