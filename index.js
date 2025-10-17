243825550db529c670656f96be99096d4508f8ce
adnanali000 <adnan.museel123@gmail.com>
2025-10-17 22:33:49 +0500

    My branch

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log(`hey `)
+let appData = {
+  members: [],
+  expenses: [],
+  nextExpenseId: 1
+};
+
+function saveData(){
+    localStorage.setItem("teamData", JSON.stringify(appData))
+    console.log("✅ Data saved to localStorage.");
+}
+
+function loadData(){
+    const storedData = localStorage.getItem("teamData")
+    if(storedData){
+        appData = JSON.parse(storedData)
+        console.log("✅ Data Loaded From LocalStorage")
+    }
+    else{
+        console.log("ℹ️ No Saved Data Found")
+    }
+}
+function clearData(){
+    localStorage.removeItem("teamData")
+    appData = {members:[], expenses: [], nextExpenseId: 1}
+    console.log("All saved data cleared from localstorage")
+}
+
+appData.members.push("Adnan")
+appData.expenses.push({id: appData.nextExpenseId++, description: "Text Expense", paidBy: "Hassan", amount: 500})
+
+saveData()
+loadData()
+
+console.table
