3ae56bf059324114e15a312cd101fce80284b561
Ismail415343 <muhammadmusa12689@gmail.com>
2025-10-17 15:33:45 +0500

    Pushed latest code for ismail-code branch

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 console.log(`hey `)
+
+
+const expenses =[
+    {name:"ismail",amount:1200},
+    {name:"haider", amount:4000},
+    {name:"tahir", amount:500},
+    {name: "sara", amount: 349}
+]
+
+let totalExpenses = 0;
+
+for(let i = 0; i<expenses.length;i++){
+totalExpenses += expenses[i].amount;
+}
+
+let memberSpendings = {};
+
+for(let i =0 ; i< expenses.length;i++){
+    let person = expenses[i].name;
+  let amount = expenses[i].amount;
+
+if(!memberSpendings[person]){
+memberSpendings[person] = 0;
+}
+
+memberSpendings[person] +=amount
+
+}
+
+let member = Object.keys(memberSpendings);
+let avgExpenses = totalExpenses/member.length;
+
+
+
+let balance = {};
+for(let i =0; i< member.length;i++){
+    let person = member[i];
+    balance[person] = memberSpendings[person] - avgExpenses;
+
+}
+
+
+console.log("========= GROUP EXPENSE REPORT =========");
+console.log(`Total Expenses = $${totalExpenses}`);
+console.log(`Average expense of per person is ${avgExpenses.toFixed(3)}\n`)
+
+
+console.log("Members Details")
+for(let i = 0; i<member.length;i++){
+    let person = member[i];
+    let spent = memberSpendings[person];
+    let bal = balance[person]
+
+
+if(bal>0){
+    console.log(`${person} spent $${spent} -> get back ${bal.toFixed(2)}\n`)
+} else if(bal<0){
+    console.log(`${person} spents $${spent} -> owes to ${Math.abs(bal).toFixed(2)}\n`)
+
+} else{
+    console.log(`${person} spents ${spent} -> settled up`)
+}
+
+
+
+
+
+
+}
